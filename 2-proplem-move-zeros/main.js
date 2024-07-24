7a0910b6b78fd5e8b2dc17ed00b537bb4e6588bd
Beshoy_Refaat <109369742+beshoyreatttt@users.noreply.github.com>
2024-07-24 18:14:37 +0000

    solution proplem 2

--- 2-proplem-move-zeros/main.js
+++ 2-proplem-move-zeros/main.js
@@ -1,28 +1,42 @@
+//first solution
 var moveZeroes = function (nums) {
-    function foundZerosIndex(nums){
-      let zeroIndexes = [];
-      if(nums == [0])
-        return nums;
-      for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == 0) {
-          zeroIndexes.push(i);
-        }
+    let newArr = [];
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] != 0) {
+        newArr.push(nums[i]);
       }
-      return zeroIndexes;
     }
-   // console.log(foundZerosIndex(nums));
-    function removeZeros(nums) {
-      let zeros = foundZerosIndex(nums);
-      for (let i = 0; i < zeros.length; i++) {
-        nums.splice(zeros[i] ,1);
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === 0) {
+        newArr.push(nums[i]);
       }
-      return nums;
     }
-    console.log(removeZeros(nums));
+    nums = newArr;
 
-
-    
-      
-    return nums;
-  };
+  return nums;
+}
   console.log(moveZeroes([0, 1, 0,0, 0, 3, 0,12]));
+
+
+//second solution
+var moveZeroes = function (nums) {
+  let numsLength = nums.length;
+  let newArr =[];
+  for (let i = 0; i < numsLength; i++) {
+    if (nums[0] === 0) {
+      nums.push(0);
+      nums.shift();
+    }else if (nums[0] !== 0) {
+      newArr.push(nums[0]);
+      nums.shift();
+    }
+    
+  }
+  newArr.reverse();
+  for (let i = 0; i < newArr.length; i++) {
+    nums.unshift(newArr[i]);
+  }
+
+return nums;
+}
+console.log(moveZeroes([0, 1, 0,0, 0, 3, 0,12]));
